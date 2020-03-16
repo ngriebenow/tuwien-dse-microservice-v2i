b1aa523d518d7307773419f55c2b120f5cc10992
Nicolas Griebenow <nicolas.griebenow@gmx.at>
2020-03-16 18:37:28 +0100

    Add Java spring boot demo project

--- src/actor-registry/src/main/java/Calculation.java
+++ src/actor-registry/src/main/java/Calculation.java
@@ -1,5 +1,9 @@
 public class Calculation {
 
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
     public static double add(double a, double b) {
         return a + b;
     }
