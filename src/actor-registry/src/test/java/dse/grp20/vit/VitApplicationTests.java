7870c20bebe7f4f47eb9134b94a78f34899c7744
Nicolas Griebenow <nicolas.griebenow@gmx.at>
2020-03-17 10:46:40 +0100

    Use SpringBoot RESTful template

---
+++ src/actor-registry/src/test/java/dse/grp20/vit/VitApplicationTests.java
@@ -0,0 +1,13 @@
+package dse.grp20.vit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
