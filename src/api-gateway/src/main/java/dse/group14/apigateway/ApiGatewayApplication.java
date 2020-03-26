d8794248d8649848308bc048f76c27f84268716b
Nicolas Griebenow <nicolas.griebenow@sap.com>
2020-03-26 22:11:55 +0100

    Add maven profiles

---
+++ src/api-gateway/src/main/java/dse/group14/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,41 @@
+package dse.group14.apigateway;
+
+import dse.group14.apigateway.filters.ErrorFilter;
+import dse.group14.apigateway.filters.PostFilter;
+import dse.group14.apigateway.filters.PreFilter;
+import dse.group14.apigateway.filters.RouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+}
