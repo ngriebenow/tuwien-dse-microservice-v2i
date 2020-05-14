d80dc5a142a091f248fd8346b9ab2f5a32c00fa4
Guenter <guenter.windsperger@gmx.at>
2020-05-14 17:52:20 +0200

    adding basic template with dependencies

---
+++ src/actor-control/src/main/java/dse/grp20/actorcontrol/ActorcontrolApplication.java
@@ -0,0 +1,13 @@
+package dse.grp20.actorcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorcontrolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActorcontrolApplication.class, args);
+    }
+
+}
---
+++ src/actor-control/src/test/java/dse/grp20/actorcontrol/ActorcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package dse.grp20.actorcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorcontrolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
