77ef715031a27632fb99146cf57a7fdd023d8ac0
thegreatkoe <l.koesslbacher@gmail.com>
2020-05-15 19:14:24 +0200

    Removed non existing exception

--- src/actor-simulator/src/test/java/dse/grp20/actorsimulator/VehicleTest.java
+++ src/actor-simulator/src/test/java/dse/grp20/actorsimulator/VehicleTest.java
@@ -1,7 +1,6 @@
 package dse.grp20.actorsimulator;
 
 
-import dse.grp20.actorsimulator.exception.NotFoundException;
 import dse.grp20.actorsimulator.service.Simulator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +15,7 @@ class VehicleTest {
 
 
 	@Test
-	public void testRegister_NewVehicle_shouldBeSaved() throws InterruptedException, NotFoundException {
+	public void testRegister_NewVehicle_shouldBeSaved() throws InterruptedException {
 
 
 	}
